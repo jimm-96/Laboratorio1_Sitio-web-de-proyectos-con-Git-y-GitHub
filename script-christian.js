30d444d8e1663efd89cb28c9cf67f81e3d0cc198
TheArsenalz4 <christiansz144@gmail.com>
2026-09-25 10:24:29 -0300

    se agrega nueva seccion de parrafo en html y se crean los elementos de la seccion con querySelector

---
+++ script-christian.js
@@ -0,0 +1,10 @@
+// =============================================
+//  script-christian.js
+//  Interactividad DOM — Sección Christian Salazar
+// =============================================
+
+// Commit 2: Seleccionar los elementos de la sección con querySelector
+const btnLeerMas = document.querySelector('#btn-leer-mas');
+const detallesExtra = document.querySelector('#detalles-extra');
+
+
